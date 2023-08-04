c8cdc5400918232c54acf092ed27f13d070cd4eb
CarlaKremer <carlakremerf@gmail.com>
2023-08-04 20:47:05 -0300

    initial config

---
+++ src/app/HomePage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default function HomePage() {
+  return (
+    <Container>
+      <p>studymate home :D</p>
+    </Container>
+  );
+}
---
+++ src/app/StudyRoom/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function StudyRoom() {
+  return <Container></Container>;
+}
