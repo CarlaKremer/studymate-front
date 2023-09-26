bfc8f02b2b0116b9b45d26cb1457d98cf6bdc731
CarlaKremer <carlakremerf@gmail.com>
2023-09-25 23:46:28 -0300

    feat: pomodo and todo basic structure

--- src/components/Pomodoro/index.tsx
+++ src/components/Pomodoro/index.tsx
@@ -1,40 +1,131 @@
-import React, { useEffect, useState } from 'react';
-
-import { Container } from './styles';
+import { useState, useLayoutEffect } from 'react'
+import { Container, Timer, ContainerButtons} from './styles'
+import Image from "next/image";
 
 export default function Pomodoro() {
-  const [counter, setCounter] = useState(25);
-  const [stop, setStop] = useState(true);
-  let counterTimer: NodeJS.Timeout;
+  let countdownTimeout: NodeJS.Timeout
 
-  useEffect(() => {
-    if (stop == false) {
-      counterTimer = setInterval(() => setCounter((pre) => pre - 1), 1000);
+  const [timeLocalStorage, setTimeLocalStorage] = useState(0)
+  const [time, setTime] = useState(25 * 60)
+  const [isActive, setIsActive] = useState(false)
+  const [rest, setRest] = useState(false)
+  const [numberSprint, setNumberSprint] = useState(1)
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+
+  function startCountdown() {
+    setIsActive(true)
+  }
+
+  function stopCountdown() {
+    clearTimeout(countdownTimeout)
+    setIsActive(false)
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout)
+    setIsActive(false)
+    if (rest) {
+      setTime(5 * 60)
+    } else {
+      setTime(25 * 60)
     }
+  }
 
-    return () => clearInterval(counterTimer);
-  });
+  function finishCountdown() {
+    if (!rest) {
+      setTime(5 * 60)
+      setRest(true)
+    } else {
+      setRest(false)
+      setTime(25 * 60)
+    }
+  }
 
-  const submitHandler = () => setStop(false);
+  useLayoutEffect(() => {
+    if (rest) {
+      if (isActive && time > 0) {
+        countdownTimeout = setTimeout(() => {
+          setTime(time - 1)
+        }, 1000)
+      } else if (isActive && time === 0) {
+        setIsActive(false)
+        setTime(25 * 60)
+        setRest(false)
+      }
+    } else {
+      if (isActive && time > 0) {
+        countdownTimeout = setTimeout(() => {
+          setTime(time - 1)
+          setTimeLocalStorage(timeLocalStorage + 1)
 
-  const resetHandler = () => {
-    setCounter(25);
-    setStop(true);
-  };
-  
-  const stopHandler = () => {
-    setStop(true);
-  };
+          // if (time === 5 * 60) {
+          //   console.log(
+          //     'Restam 5 minutos'
+          //   )
+          // }
+        }, 1000)
+      } else if (isActive && time === 0) {
+        setIsActive(false)
+        setTime(5 * 60)
+        setRest(true)
+        setNumberSprint(numberSprint + 1)
+      }
+    }
+  }, [isActive, time])
 
-return (
-  <Container>
-    <>
-      <p>{counter}</p>
+  return (
+    <Container>
+      <Timer>
+        <span>{minuteLeft}</span>
+        <span>{minuteRight}</span>
+        <p>:</p>
+        <span>{secondLeft}</span>
+        <span>{secondRight}</span>
+      </Timer>
 
-      <button onClick={submitHandler}>Start</button>
-      <button onClick={stopHandler}>Stop</button>
-      <button onClick={resetHandler}>Reset</button>
-    </>
-  </Container>
-);
+      <ContainerButtons active={isActive}>
+        {isActive ? (
+          <button className="stop" onClick={stopCountdown}>
+             <Image
+              src={"./assets/icons/pause.svg"}
+              width={16}
+              height={16}
+              alt="ícone de parar"
+            />
+          </button>
+        ) : (
+          <button className="start" onClick={startCountdown}>
+             <Image
+              src={"./assets/icons/play.svg"}
+              width={16}
+              height={16}
+              alt="ícone de play"
+            />
+          </button>
+        )}
+        <button className="reset" onClick={() => resetCountdown()}>
+            <Image
+              src={"./assets/icons/backward.svg"}
+              width={16}
+              height={16}
+              alt="ícone de resetar"
+            />
+        </button>
+
+        <button className="finish" onClick={() => finishCountdown()}>
+            <Image
+              src={"./assets/icons/stop.svg"}
+              width={16}
+              height={16}
+              alt="ícone de parar"
+            />
+        </button>
+      </ContainerButtons>
+    </Container>
+  )
 }
--- src/components/Pomodoro/styles.ts
+++ src/components/Pomodoro/styles.ts
@@ -1,4 +1,94 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
+
+type Props = {
+  active: any
+}
 
 export const Container = styled.div`
-`;
+  height: 100%;
+
+  margin: 0 auto;
+  padding: 25px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  h1 {
+    color: #2e384d;
+  }
+  
+  h2 {
+    color: #b3b3b3;
+    font-weight: bold;
+  }
+`
+
+
+export const Timer = styled.div`
+  margin: 1rem 0 1rem 0;
+
+  display: flex;
+  align-items: center;
+
+  line-height: 0rem;
+  font-size: 2rem;
+  color: #fafafa;
+`
+
+export const ContainerButtons = styled.div<Props>`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+
+  button {
+    padding: 15px 15px;
+    font-size: 1rem;
+
+    border: 1px solid #FAFAFA;
+    border-radius: 0.75rem;
+    color: #fff;
+
+    transition: all 0.2s;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .start {
+    background: var(--green);
+
+    &:hover {
+      background: var(--darkgreen);
+    }
+  }
+
+  .stop {
+    background: var(--red);
+
+    &:hover {
+      background: var(--darkred);
+    }
+  }
+
+  .reset {
+    background: rgba(90, 90, 90, 0.6);
+    
+    &:hover {
+      background: rgba(90, 90, 90, 0.9);
+    }
+  }
+
+  .finish {
+    background: darkred;
+    
+    &:hover {
+      background: rgba(50, 50, 50, 1);
+    }
+  }
+`
