b5d381ba8d09ad44f211170df44aae577800aa05
NatanDuarte <natanprocopio@gmail.com>
2023-11-20 20:28:33 -0300

    change: custom conference component

--- src/components/VideoCall/index.tsx
+++ src/components/VideoCall/index.tsx
@@ -1,12 +1,15 @@
 import { use, useContext, useEffect, useRef, useState } from "react";
 import '@livekit/components-styles';
 import {
-    ControlBar,
-    GridLayout,
-    LiveKitRoom,
-    ParticipantTile,
-    RoomAudioRenderer,
-    useTracks,
+  ControlBar,
+  GridLayout,
+  LiveKitRoom,
+  TrackRefContext,
+  isTrackReference,
+  VideoTrack,
+  TrackMutedIndicator,
+  RoomAudioRenderer,
+  useTracks,
 } from '@livekit/components-react';
 import { Track } from "livekit-client";
 
@@ -15,43 +18,52 @@ const serverUrl = 'ws://localhost:7880';
 
 export default function VideoCall({ token }: { token: string }) {
 
-    return (
-        <LiveKitRoom
-            video={false}
-            audio={false}
-            token={token}
-            connectOptions={{ autoSubscribe: false }}
-            serverUrl={serverUrl}
-            // Use the default LiveKit theme for nice styles.
-            data-lk-theme="default"
-            style={{ height: '100vh', backgroundColor: 'transparent' }}
-        >
-            {/* Your custom component with basic video conferencing functionality. */}
-            <MyVideoConference />
-            {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
-            <RoomAudioRenderer />
-            {/* Controls for the user to start/stop audio, video, and screen 
+  return (
+    <LiveKitRoom
+      video={false}
+      audio={false}
+      token={token}
+      serverUrl={serverUrl}
+      // Use the default LiveKit theme for nice styles.
+      data-lk-theme="default"
+      style={{
+        height: '100%',
+        backgroundColor: 'transparent'
+      }}
+    >
+      {/* Your custom component with basic video conferencing functionality. */}
+      <MyVideoConference />
+      {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+      <RoomAudioRenderer />
+      {/* Controls for the user to start/stop audio, video, and screen 
       share tracks and to leave the room. */}
-            <ControlBar />
-        </LiveKitRoom>
-    );
+      <ControlBar />
+    </LiveKitRoom>
+  );
 }
 
 const MyVideoConference = () => {
-    // `useTracks` returns all camera and screen share tracks. If a user
-    // joins without a published camera track, a placeholder track is returned.
-    const tracks = useTracks(
-        [
-            { source: Track.Source.Camera, withPlaceholder: true },
-            { source: Track.Source.Microphone, withPlaceholder: true },
-        ],
-    );
-    return (
-        <GridLayout tracks={tracks}
-            style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
-            {/* The GridLayout accepts zero or one child. The child is used
-      as a template to render all passed in tracks. */}
-            <ParticipantTile />
-        </GridLayout>
-    );
+  const tracks = useTracks([
+    { source: Track.Source.Camera, withPlaceholder: true },
+    { source: Track.Source.ScreenShare, withPlaceholder: false },
+  ]);
+  return (
+    <>
+      <GridLayout tracks={tracks}>
+        <TrackRefContext.Consumer>
+          {(track) =>
+            track && (
+              <div className="my-tile">
+                {isTrackReference(track) ? <VideoTrack {...track} /> : <div></div>}
+                <div style={{ display: 'flex' }}>
+                  <TrackMutedIndicator source={Track.Source.Microphone} />
+                  <TrackMutedIndicator source={track.source} />
+                </div>
+              </div>
+            )
+          }
+        </TrackRefContext.Consumer>
+      </GridLayout>
+    </>
+  );
 }
