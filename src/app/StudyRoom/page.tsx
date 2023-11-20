40cfbab71a5959db4b83a1bdf169e2ac1fbd53d5
NatanDuarte <natanprocopio@gmail.com>
2023-11-20 16:32:32 -0300

    feat/change: VideoCall component powered by livekit + fix broken chat

--- src/app/StudyRoom/page.tsx
+++ src/app/StudyRoom/page.tsx
@@ -10,21 +10,45 @@ import Image from "next/image";
 import Todo from "@/components/Todo";
 import Pomodoro from "@/components/Pomodoro";
 import { useSearchParams } from 'next/navigation';
-import ScreenSharing from "@/components/ScreenSharing";
+import VideoCall from "@/components/VideoCall";
+import Loading from "@/components/Loading";
+import api from "../../service/api";
 
 
 export default function StudyRoom() {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
+  const [liveKitToken, setLiveKitToken] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const [isOpen, setIsOpen] = useState(true);
   const [userLogged, setUserLogged] = useState<any>(null);
 
   const getRoomId = () => {
-    return searchParams.get('roomId')
+    return searchParams.get('roomId');
+  }
+
+  const requestLivekitToken = async () => {
+    setLoading(true);
+    try {
+      const raw = JSON.stringify({
+        userName: userLogged,
+        roomName: getRoomId(),
+      });
+
+      const token = localStorage.getItem('access_token')?.replaceAll('"', "")
+
+      const res = await api
+        .post("/livekit-auth", raw, { headers: { 'Authorization': "Bearer " + token } });
+
+      setLiveKitToken(String(res.data.token));
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
   }
 
   useEffect(() => {
-
+    requestLivekitToken();
     const sessionStorageUser = sessionStorage.getItem("username");
     if (sessionStorageUser != null) {
       setUserLogged(JSON.parse(sessionStorageUser));
@@ -32,7 +56,6 @@ export default function StudyRoom() {
   }, []);
   return (
     <Container>
-
       <Column>
         <Navigation>
           <Link href="/HomePage">
@@ -45,9 +68,9 @@ export default function StudyRoom() {
             />
           </Link>
         </Navigation>
-        <ScreenSharing
-          roomId={getRoomId()}
-          server='http://localhost:3090' />
+        {loading ? <Loading /> : (
+          <VideoCall token={liveKitToken} />
+        )}
         <RoomMates />
       </Column>
 
